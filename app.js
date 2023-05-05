2f2b54eca35f40e160dea7045606d251eddcaf95
Nwachee <fuzzy245.in@gmail.com>
2023-05-05 02:25:50 +0100

    New changes implemented

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const dotenv = require('dotenv')
+dotenv.config({ path: './.env' })
+const connectDB = require('./src/DB/connect');
+const rootRoute = require('./src/Routes/index.route')
+
 const app = express();
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+//Root Route
+app.use('/api/v1', rootRoute)
 
 
 const port = process.env.PORT || 5000
+const start = (async () => {
+	await connectDB(process.env.MONGO_URI);
 
+	app.listen(port, () => {
+		console.log(`Server is listening at ${port}...`);
+	});
+});
 
-
-app.listen(port, () =>
-console.log(`Server is listening on port ${port}...`)
-);
+start();
